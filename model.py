5ec703c328b16f97d2e89e2cd7f8b1614689d77f
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2019-11-15 21:05:48 +0200

    Very first implementation

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import os
 import csv
 import cv2
 import numpy as np
@@ -9,11 +8,9 @@ from sklearn.model_selection import train_test_split
 
 from keras.models import Sequential
 from keras.layers import Lambda, Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Cropping2D
-from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-from keras.utils.vis_utils import plot_model
 
 
-def generator(samples, batch_size=32):
+def generator(samples, batch_size=32, path_to_images='data/IMG/'):
     num_samples = len(samples)
     while 1:  # Loop forever so the generator never terminates
         shuffle(samples)
@@ -23,20 +20,33 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = './IMG/'+batch_sample[0].split('/')[-1]
+                # center img
+                name = path_to_images + batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
                 angles.append(center_angle)
 
-            # trim image to only see section with road
+                # left img
+                name = path_to_images + batch_sample[1].split('/')[-1]
+                left_image = cv2.imread(name)
+                images.append(left_image)
+                angles.append(center_angle + 0.2)
+
+                # right img
+                name = path_to_images + batch_sample[2].split('/')[-1]
+                right_image = cv2.imread(name)
+                images.append(right_image)
+                angles.append(center_angle - 0.2)
+
             X_train = np.array(images)
             y_train = np.array(angles)
+
             yield sklearn.utils.shuffle(X_train, y_train)
 
 
 samples = []
-with open('./driving_log.csv') as csvfile:
+with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
@@ -50,15 +60,34 @@ batch_size = 32
 train_generator = generator(train_samples, batch_size=batch_size)
 validation_generator = generator(validation_samples, batch_size=batch_size)
 
-ch, row, col = 3, 80, 320  # Trimmed image format
-
 model = Sequential()
-# Preprocess incoming data, centered around zero with small standard deviation
-model.add(Lambda(lambda x: x/127.5 - 1.,
-                 input_shape=(ch, row, col),
-                 output_shape=(ch, row, col)))
 
-# model.add(... finish defining the rest of your model architecture here ...)here
+# Preprocess incoming data, centered around zero with small standard deviation
+model.add(Lambda(lambda x: x/127.5 - 0.5, input_shape=(160, 320, 3)))
+
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+
+# Conv2D layer
+model.add(Conv2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(MaxPooling2D())
+model.add(Dropout(0.25))
+
+# Conv2D layer
+model.add(Conv2D(32, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(MaxPooling2D())
+
+# Flattening
+model.add(Flatten())
+
+# Dense layer
+model.add(Dense(32))
+model.add(Dropout(0.20))
+
+# Dense Layer
+model.add(Dense(16))
+
+# Dense Layer
+model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator,
@@ -67,3 +96,5 @@ model.fit_generator(train_generator,
                     validation_steps=np.ceil(len(validation_samples)/batch_size),
                     epochs=5,
                     verbose=1)
+
+model.save('model.h5')
